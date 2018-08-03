5821211dd700c791d8f2288a4809dcc1983f486e
Hikazoh <hikazoh.gmail.com>
2018-08-03 13:20:03 +0900

    async

--- src/async/core.clj
+++ src/async/core.clj
@@ -1,18 +1,20 @@
 (ns async.core
   (:gen-class)
-  (:require [clojure.core.async :refer [chan go-loop >! <! timeout] :as async])
+  (:require [clojure.core.async :refer [chan go-loop go >! <! timeout] :as async])
   (:import (javax.swing JFrame JButton JLabel JOptionPane)
            (java.awt BorderLayout)
-           (java.awt.event ActionListener)))
+           (java.awt.event ActionListener)
+           (java.util Date)))
 
 (defn -main
   "Java Swing & core.async sample"
   [& args]
   (def ch (chan))
+  (def data (ref (.toString (Date.))))
   (def frame
     (proxy [JFrame] []
       (paint[g]
-        (.drawString  g "aaa" 100 100))))
+        (.drawString  g @data 10 10))))
   (.setTitle frame "Hikazoh's Title")
   (.setSize frame 200 200 )
   (.setLocationRelativeTo frame nil)
@@ -21,11 +23,16 @@
   (.addActionListener button
    (reify ActionListener
      (actionPerformed [this e]
-       (JOptionPane/showMessageDialog frame (JLabel. "Clicked!!")))))
+       (do
+         (JOptionPane/showMessageDialog frame (JLabel. "Clicked!!"))
+         (go
+           (>! ch "Clicked"))))))
   (.add frame button BorderLayout/NORTH)
   (.setVisible frame true)
   (go-loop[]
-    (when-let [d (<! char)]
+    (when-let [d (<! ch)]
+      (dosync (ref-set data d))
       (.repaint frame 1 0 0 200 200)
       (recur))))
+
 
